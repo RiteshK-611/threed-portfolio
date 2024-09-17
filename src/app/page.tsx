96a34c84f3d28b0d6b242a61786d75f3214c306c
Ritesh <riteshkokam@gmail.com>
2024-09-17 20:12:19 +0530

    Add Hero section and related components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import Navbar from "./sections/Navbar"
+import Navbar from "@/sections/Navbar";
+import Hero from "@/sections/Hero";
 
 export default function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 }

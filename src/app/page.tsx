c2503ccc65278d66e6f739f3539d7cc40356824e
Ritesh <riteshkokam@gmail.com>
2024-09-17 22:27:39 +0530

    Add About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
+import About from "@/sections/About";
 
 export default function App() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <About />
     </main>
   );
 }

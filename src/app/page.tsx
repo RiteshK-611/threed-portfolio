451b235979775386eadfcda0bf08bc73d8afabc4
riteshkokam <riteshkokam@gmail.com>
2024-09-18 17:19:05 +0000

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
 import About from "@/sections/About";
+import Projects from "@/sections/Projects";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </main>
   );
 }

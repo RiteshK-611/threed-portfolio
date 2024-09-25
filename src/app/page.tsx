b620b7ddb7d5150621294b46fc2673349062f703
riteshkokam <riteshkokam@gmail.com>
2024-09-25 18:21:59 +0000

    Add client to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
 import About from "@/sections/About";
 import Projects from "@/sections/Projects";
+import Clients from "@/sections/Projects"
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Hero />
       <About />
       <Projects />
+      <Clients />
     </main>
   );
 }

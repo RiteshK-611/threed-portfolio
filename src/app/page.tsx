afda9e73df6a366240bd168dfd53ffdc3e819469
Ritesh Kokam <riteshkokam@gmail.com>
2024-09-28 13:41:28 +0000

    Add Footer to page and Fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
 import About from "@/sections/About";
 import Projects from "@/sections/Projects";
+import Clients from "@/sections/Clients";
+import WorkExperience from "@/sections/Experience";
+import Contact from "@/sections/Contact";
+import Footer from "@/sections/Footer";
 
 export default function App() {
   return (
@@ -11,6 +15,10 @@ export default function App() {
       <Hero />
       <About />
       <Projects />
+      <Clients />
+      <WorkExperience />
+      <Contact />
+      <Footer />
     </main>
   );
 }

f44e15ece65155957e6cdb50212cee4d0f5f7056
Ritesh Kokam <riteshkokam@gmail.com>
2024-10-19 14:23:45 +0000

    Update Navbar and constants

--- src/sections/Navbar1.jsx
+++ src/sections/Navbar1.jsx
@@ -11,16 +11,16 @@ export default function Navbar1() {
         direction="middle"
         className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] "
       >
-        {Object.entries(navLinks).map(([name, nav]) => (
+        {navLinks.map(({ name, href, icon }) => (
           <DockIcon key={name}>
             <Link
-              href={nav.url}
+              href={href}
               className={cn(
                 buttonVariants({ variant: "ghost", size: "icon" }),
                 "rounded-full"
               )}
             >
-              <nav.icon className="size-5" />
+              <img src={icon} className="size-5 text-white" />
             </Link>
           </DockIcon>
         ))}

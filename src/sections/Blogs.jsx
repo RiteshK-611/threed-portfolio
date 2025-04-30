1a77b37cd158eda6ba4206b37d323ada9bafdf57
Ritesh Kokam <61982298+RiteshK-611@users.noreply.github.com>
2025-05-01 01:46:33 +0530

    Refactors blog to fetch Dev.to (#77)

--- src/sections/Blogs.jsx
+++ src/sections/Blogs.jsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 import axios from "axios";
 
 const BlogCard = ({ post }) => {
-  const { node } = post;
   const { setIsHovering, setText } = useGlowingBall();
 
   const handleMouseEnter = () => {
@@ -33,28 +32,26 @@ const BlogCard = ({ post }) => {
         },
         hidden: { opacity: 0, y: 50 },
       }}
-      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-    >
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
       <Link
-        href={node.url}
+        href={post.url}
         target="_blank"
         rel="noopener noreferrer"
         className="bg-tertiary rounded-[20px] px-2 min-h-[280px] flex flex-col cursor-pointer group"
         onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
+        onMouseLeave={handleMouseLeave}>
         <div className="aspect-[1.9/1] mb-4 overflow-hidden rounded-lg">
           <img
-            src={node.coverImage.url}
-            alt={node.title}
+            src={post.social_image}
+            alt={post.title}
             className="rounded-lg w-full h-full object-cover group-hover:scale-105 transition-all duration-500 ease-in-out"
           />
         </div>
         <h3 className="text-white font-bold text-xl md:text-2xl">
-          {node.title}
+          {post.title}
         </h3>
         <p className="text-white-600 text-base mt-2 line-clamp-3">
-          {node.brief}
+          {post.description}
         </p>
         <p className="mt-4 text-white-700 font-semibold self-start hover:underline flex items-center gap-2">
           Read More{" "}
@@ -74,62 +71,24 @@ const Blogs = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchDevToPosts = async () => {
       try {
-        const query = `
-          query {
-            publication(host: "riteshkokam.hashnode.dev") {
-              posts(first: 3) {
-                edges {
-                  node {
-                    coverImage {
-                      url
-                    }
-                    title
-                    brief
-                    url
-                    slug          
-                    publishedAt
-                  }
-                }
-              }
-            }
-          }
-        `;
-
-        const response = await axios.post("https://gql.hashnode.com/", {
-          headers: {
-            "Content-Type": "application/json",
+        const response = await axios.get("/api/devto/articles", {
+          params: {
+            per_page: 3,
+            username: "riteshkokam",
           },
-          query: query,
         });
-        console.log("hashnode response", response);
-        setPosts(response.data.data.publication.posts.edges);
+
+        console.log("devto response", response);
+        setPosts(response.data);
       } catch (error) {
-        console.error("Error fetching Hashnode posts:", error);
+        console.error("Error fetching Dev.to posts:", error);
       } finally {
         setLoading(false);
       }
     };
 
-    const fetchDevToPosts = async () => {
-      try {
-        console.log(process.env.DEV_TO_API_KEY);
-        const response = await axios.get("/api/devto/articles/me/published", {
-          headers: {
-            Accept: "application/vnd.forem.api-v1+json",
-            "api-key": process.env.DEV_TO_API_KEY,
-          },
-        });
-
-        console.log("devto response", response);
-        return response.data; // implement the logic to set post from dev.to same like hashnode
-      } catch (error) {
-        console.error("Error fetching Dev.to posts:", error);
-        return [];
-      }
-    };
-    fetchPosts();
     fetchDevToPosts();
   }, []);
 
@@ -151,12 +110,11 @@ const Blogs = () => {
           hidden: {
             opacity: 0,
           },
-        }}
-      >
+        }}>
         {loading ? (
           <p className="text-secondary">Loading blog posts...</p>
         ) : posts.length > 0 ? (
-          posts.map((post) => <BlogCard key={post.node.slug} post={post} />)
+          posts.map((post) => <BlogCard key={post.id} post={post} />)
         ) : (
           <p className="text-secondary">No blog posts found.</p>
         )}
